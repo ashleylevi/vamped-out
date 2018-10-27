53fe74e5a96ab833f743ca98b6e17e37bbd35e93
Kylie <knstanley11@gmail.com>
2018-10-26 20:44:34 -0600

    Add props parameter and WatchlistCard for rendering

--- src/Watchlist.js
+++ src/Watchlist.js
@@ -3,11 +3,11 @@ import './Watchlist.css';
 import WatchlistCard from './WatchlistCard.js';
 
 
-export default function Watchlist() {
+export default function Watchlist(props) {
   return (
       <div className="watchlist">
         <ul>
-          
+          <WatchlistCard episode={props.clickedCards}/>
         </ul>
       </div>
     );
