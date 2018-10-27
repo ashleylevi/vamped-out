e37a310f4e604bb5b661d5dee4754748a0cfd465
Kylie <knstanley11@gmail.com>
2018-10-26 21:52:41 -0600

    Add watch list header and wrapper for styling carousel

--- src/Watchlist.js
+++ src/Watchlist.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import './Watchlist.css';
 import WatchlistCard from './WatchlistCard.js';
+import './Watchlist.css';
 
 
 export default function Watchlist(props) {
   return (
+    <div className="watchlist-wrapper">
+      <h2>Your Watch List</h2>
       <div className="watchlist">
         <ul>
-        {
-          props.clickedCards.map(card => {
-            return <WatchlistCard clickedCards={props.clickedCards} />
-          })
-        }
+          {
+            props.clickedCards.map(card => {
+              console.log(card)
+              return <WatchlistCard card={card} />
+            })
+          }
         </ul>
       </div>
-    );
-  }
+    </div>
+  );
+}
