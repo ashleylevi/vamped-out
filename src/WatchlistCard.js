b272a81b311acbbb026f7198dd99c03899e5a361
Kylie <knstanley11@gmail.com>
2018-10-26 21:15:49 -0600

    Pass props into WatchlistCard and render props in li

--- src/WatchlistCard.js
+++ src/WatchlistCard.js
@@ -2,14 +2,13 @@ import React, { Component } from 'react';
 import './WatchlistCard.css';
 
 export default class WatchlistCard extends Component {
-  constructor () {
-    super();
+  constructor (props) {
+    super(props);
   }
   render() {
     return (
       <li className="watchlist-card">
-        
-        
+      {this.props.clickedCards.name} 
       </li>
     );
   }
