66fb61c3a2ac1ec4db190770ba0849ce74bd7c52
Kylie <knstanley11@gmail.com>
2018-10-28 18:57:51 -0600

    Add buttons to watchlist carousel

--- src/Watchlist.js
+++ src/Watchlist.js
@@ -8,6 +8,7 @@ export default function Watchlist(props) {
   return (
     <div className="watchlist-wrapper">
       <h2>Your Watch List</h2>
+      <button className="button left-button"><i className="fas fa-angle-left" onClick={props.shiftCarousel}></i></button>
       <div className="watchlist">
         <ul>
           {
@@ -18,6 +19,7 @@ export default function Watchlist(props) {
           }
         </ul>
       </div>
+        <button className="button right-button"><i className="fas fa-angle-right"onClick={props.shiftCarousel}></i></button>
     </div>
   );
 }
