f1a1a2216c655d23060adcadcc768587b61e5fbf
Kylie <knstanley11@gmail.com>
2018-10-31 20:23:42 -0600

    refactor watchlist

--- src/Watchlist.js
+++ src/Watchlist.js
@@ -2,23 +2,26 @@ import React from 'react';
 import WatchlistCard from './WatchlistCard.js';
 import './main.scss';
 
-
 export default function Watchlist(props) {
   return (
     <div className="watchlist-wrapper">
       <h2 className="watchlist-header">Your Watchlist</h2>
-      <button className="button watchlist-button left-button"><i className="fas fa-angle-left" onClick={props.shiftWatchlist}></i></button>
+      <button className="button watchlist-button left-button">
+        <i className="fas fa-angle-left" onClick={props.shiftWatchlist}></i>
+      </button>
       <div className="watchlist">
         <ul>
           {
             props.clickedCards.map((card, index) => {
               return <WatchlistCard key={index} card={card}
-                      removeFromWatchlist={props.removeFromWatchlist} />
+                                    removeFromWatchlist={props.removeFromWatchlist} />
             })
           }
         </ul>
       </div>
-        <button className="button watchlist-button right-button"><i className="fas fa-angle-right"onClick={props.shiftWatchlist}></i></button>
+      <button className="button watchlist-button right-button">
+        <i className="fas fa-angle-right"onClick={props.shiftWatchlist}></i>
+      </button>
     </div>
   );
 }
