caa34513b5182628954d3dd1c11e594e27f129e9
Kylie <knstanley11@gmail.com>
2018-10-31 20:22:33 -0600

    refactor search

--- src/Search.js
+++ src/Search.js
@@ -2,21 +2,12 @@ import React, { Component } from 'react';
 import './main.scss';
 import Trie from '@ashleyplevi/autocomplete';
 
-//instantiate a new tree
-//populate it with (start with random words) whatever data a user can search for by typing in the search
-//map over dataset and just pull out what you want user to search for(example name, weapon) and populate your tree with that info 
-//integrate tree suggestions into search functionality 
-//keep track of what user types in. This will be prefix for suggest method on tree
-//store suggestions array in state
-//only call populate and suggest from trie
-
 export default class Search extends Component {
   constructor(props) {
     super(props);
     this.state = {
       seachValue: '',
       suggestedWords: [],
-
     }
   }
 
@@ -27,20 +18,17 @@ export default class Search extends Component {
     })
 
     this.props.filterEpisodes(e.target.value);
-
   } 
-
 
   render() {
     return (
       <div className="search-bar-container">
         <input type="text" list="characters" placeholder="  Search for your favorite Buffy or Angel episodes" onKeyUp={this.handleState}/>
         <datalist id="characters">
-          { this.state.suggestedWords.map((word) => {
-            return  <option value={word} />
-
-          })
-           
+          {
+            this.state.suggestedWords.map((word) => {
+              return  <option value={word} />
+            })
           }
         </datalist>
       </div>
